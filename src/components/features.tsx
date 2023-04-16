ddc8efd1286c66747bb0b8983f5ed425d9a253c0
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-04-16 19:32:29 +0100

    style: better popping colors

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -9,6 +9,7 @@ import {
   createStyles,
   rem,
   useMantineTheme,
+  type MantineTheme,
 } from "@mantine/core";
 import {
   IconAward,
@@ -31,7 +32,8 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-const features = [
+const features = (theme: MantineTheme) => [
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
   {
     icon: IconReportMoney,
     title: "Master your budget",
@@ -53,26 +55,43 @@ const features = [
   {
     icon: IconFlame,
     title: "Discover, share, and save",
-    description:
-      "Experience the full power of 'Do I Really Need This?' with additional features like prioritization, social engagement, price tracking, informed decision-making, gamification, and valuable insights into your wishlist habits.",
+    description: (
+      <>
+        Experience the full power of{" "}
+        <span
+          style={{
+            fontWeight: 700,
+            color:
+              theme.colorScheme === "dark" ? "white" : theme.colors.gray[9],
+          }}
+        >
+          do i really need this?
+        </span>{" "}
+        with additional features like prioritization, social engagement, price
+        tracking, informed decision-making, gamification, and valuable insights
+        into your wishlist habits.
+      </>
+    ),
   },
+  /* eslint-enable @typescript-eslint/no-unsafe-assignment */
 ];
 
 export function Features() {
   const { classes } = useStyles();
   const theme = useMantineTheme();
+  const isDark = theme.colorScheme === "dark";
 
-  const items = features.map((feature) => (
+  const items = features(theme).map((feature) => (
     <div key={feature.title}>
       <ThemeIcon
         size={44}
         radius="md"
         variant="outline"
-        color={theme.colorScheme === "dark" ? "gray" : "dark"}
+        color={isDark ? "gray" : "dark"}
       >
         <feature.icon size={rem(26)} stroke={1.5} />
       </ThemeIcon>
-      <Text fz="lg" mt="sm" fw={500}>
+      <Text fz="lg" mt="sm" fw={500} color={isDark ? "white" : "dark"}>
         {feature.title}
       </Text>
       <Text c="dimmed" fz="sm">
@@ -91,17 +110,29 @@ export function Features() {
           <Text c="dimmed">
             Discover the smarter way to manage your wants and budget with our
             handy web app designed to make your wishlist journey seamless and
-            enjoyable. 'Do I Really Need This?' brings together an array of
-            features to help you stay organized, make informed decisions, and
-            keep your spending in check.
+            enjoyable.{" "}
+            <span
+              style={{
+                fontWeight: 700,
+                color: isDark ? "white" : theme.colors.gray[9],
+              }}
+            >
+              do i really need this?
+            </span>{" "}
+            brings together an array of features to help you stay organized,
+            make informed decisions, and keep your spending in check.
           </Text>
 
           <Button
-            variant="gradient"
-            gradient={{ deg: 133, from: "blue", to: "cyan" }}
-            size="lg"
-            radius="md"
+            variant="outline"
+            color={isDark ? "gray" : "dark"}
             mt="xl"
+            radius="md"
+            size="xl"
+            // cool rainbow background gradient
+            // style={{
+            //   backgroundImage: `background-image: linear-gradient(273.64deg,#35c3f3 11.27%,#8b9fe8 36.72%,#e681d8 61.72%,#ffa9a4 82.79%,#fed2ce 107.29%,#fed2ce 107.29%)`,
+            // }}
           >
             Get started
           </Button>
